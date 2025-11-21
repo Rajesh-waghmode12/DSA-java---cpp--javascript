09c3757a6f91a6acacb1f2e427c0bbf90e9b95bb
Rajesh-Waghmode <garryneon07@gmail.com>
2025-11-21 19:29:34 +0530

    java class added for singly linked list

---
+++ Linked List/SinglyLinkedList.java
@@ -0,0 +1,32 @@
+
+class Node {
+    
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+
+
+
+public class SinglyLinkedList {
+
+
+    public static void insetAHead(Node head, int data) {
+        Node temp = head;
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        Node head = new Node(23);
+
+
+        
+    }
+}
